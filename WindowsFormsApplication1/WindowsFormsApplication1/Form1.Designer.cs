290909ec718952ed1936544bfdb6116ab023b800
Gui <Gui@DESKTOP-TDATJTN>
2019-10-22 18:48:49 -0300

    Modifique el boton

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,8 +33,9 @@
             // 
             // button1
             // 
+            this.button1.Enabled = false;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(70, 82);
+            this.button1.Location = new System.Drawing.Point(79, 82);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 90);
             this.button1.TabIndex = 0;
